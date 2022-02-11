ba308a225e275fd1fdcaee16311fca0c91449b1f
NewYorkSteak <46742223+NewYorkSteak@users.noreply.github.com>
2022-02-11 15:58:56 -0800

    Add files via upload

---
+++ DS18B20.h
@@ -0,0 +1,17 @@
+/*
+ * DS18B20.h
+ *
+ *  Created on: Feb 2, 2022
+ *      Author: Michael
+ */
+
+#ifndef SRC_DS18B20_H_
+#define SRC_DS18B20_H_
+
+void setOutDS(GPIO_TypeDef* GPIOx);
+void setInDS(GPIO_TypeDef* GPIOx);
+uint8_t dsInit();
+void dsWrite(uint8_t data);
+uint8_t dsRead();
+
+#endif /* SRC_DS18B20_H_ */
---
+++ delay.h
@@ -0,0 +1,14 @@
+/*
+ * delay.h
+ *
+ *  Created on: Feb 2, 2022
+ *      Author: Michael
+ */
+
+#ifndef SRC_DELAY_H_
+#define SRC_DELAY_H_
+
+#include "stm32l4xx_hal.h"
+void delay(uint16_t time);
+
+#endif /* SRC_DELAY_H_ */
