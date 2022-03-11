89c0cbecb9d00a400f24ebe6d990fb57c601c271
Alejandro Diera <46726478+alejandrodiera@users.noreply.github.com>
2022-03-11 13:38:10 -0800

    Create HC05_ATCOMMAND.ino
    Code to pair the HC05 with an ESP32 over BT. Uses SoftwareSerial library.

---
+++ HC05_ATCOMMAND.ino
@@ -0,0 +1,31 @@
+/*
+ ARDUINO UNO ELEGOO - HC05 Bluetooth Module
+ Software serial multple serial test
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+ The circuit:
+ RX is digital pin 2 (connect to TX of other device)
+ TX is digital pin 3 (connect to RX of other device)
+*/
+
+// Target ESP32 LoRa Gateway BT Mac Address = a8:03:2a:f2:74:2a
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(11, 10); // RX, TX
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600); //Serial monitor needs to be set to Both NL & CR, 9600 baud.
+  mySerial.begin(38400);
+  Serial.println("***AT COMMAND MODE***");
+  mySerial.println("Hello, world! - mySerial");
+}
+
+void loop() { // run over and over
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+} 
