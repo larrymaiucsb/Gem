1fe8b68449bd0fba4c57affd13fecda32c598170
Alejandro Diera <46726478+alejandrodiera@users.noreply.github.com>
2022-03-11 13:40:45 -0800

    Newest Push - 3/11/2022
    Code to make the HC05 into AT Command Mode. Make sure to pull EN to 5V & press the physical button on the HC05 when powering on. Uses SoftwareSerial library for UART connectivity.

--- HC05_AT_COMMAND_MODE.ino
+++ HC05_AT_COMMAND_MODE.ino
@@ -4,8 +4,8 @@
  Receives from the hardware serial, sends to software serial.
  Receives from software serial, sends to hardware serial.
  The circuit:
- RX is digital pin 2 (connect to TX of other device)
- TX is digital pin 3 (connect to RX of other device)
+ RX is digital pin 11 (connect to TX of other device)
+ TX is digital pin 10 (connect to RX of other device)
 */
 
 // Target ESP32 LoRa Gateway BT Mac Address = a8:03:2a:f2:74:2a
